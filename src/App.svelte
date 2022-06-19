a60a9b715447f643b3008373c7f74797b47729d0
Chris Arnold <carnold84@users.noreply.github.com>
2022-06-19 22:07:36 +1200

    Fixes (#3)

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import Editor from './components/Editor.svelte';
   import AddIcon from './icons/AddIcon.svelte';
   import CompleteIcon from './icons/CompleteIcon.svelte';
+  import MenuIcon from './icons/MenuIcon.svelte';
   import RemoveIcon from './icons/RemoveIcon.svelte';
   import SavingIcon from './icons/SavingIcon.svelte';
   import notesStore, {
@@ -11,40 +12,57 @@
   } from './stores/notesStore';
 
   let editedNote;
+  let isMenuVisible = false;
+  let isSaving = false;
   let notes = [];
-  let saving = false;
   let timeoutId = null;
   let title;
+
+  const onToggleMenu = () => {
+    isMenuVisible = !isMenuVisible;
+  };
 
   const onSelectNote = (note) => {
     if (note) {
       editedNote = { ...note };
       title = editedNote.title;
     }
+
+    isMenuVisible = false;
   };
 
   const onAddNote = () => {
-    addNote();
+    const note = addNote();
+
+    onSelectNote(note);
+    isMenuVisible = false;
   };
 
   const onRemoveNote = (note) => {
     removeNote(note);
+
+    if (note.id === editedNote.id) {
+      onSelectNote(undefined);
+    }
   };
 
   const debouncedUpdateNote = (data) => {
     clearTimeout(timeoutId);
 
     timeoutId = setTimeout(() => {
+      isSaving = true;
+
       updateNote({
         ...editedNote,
         ...data,
       });
-      saving = false;
+      setTimeout(() => {
+        isSaving = false;
+      }, 500);
     }, 2000);
   };
 
   const onChange = (evt) => {
-    saving = true;
     let update;
 
     if (evt.detail) {
@@ -71,73 +89,98 @@
   });
 </script>
 
-<main class="absolute flex h-full w-full overflow-hidden bg-neutral-900">
-  <div class="w-5/12 lg:w-4/12 xl:w-3/12 h-full flex flex-col bg-neutral-800">
-    <div
-      class="flex flex-shrink-0 items-center h-14 border-b border-neutral-500 px-5"
-    >
-      <h1 class="flex-grow text-xl text-zinc-400">Notes</h1>
+<main
+  class="absolute flex flex-col h-full w-full overflow-hidden bg-neutral-900"
+>
+  <div
+    class="flex flex-shrink-0 items-center justify-between h-14 bg-neutral-700 px-5"
+  >
+    <div class="flex">
       <button
-        class="text-md text-zinc-500 hover:text-zinc-300"
-        title="New note"
-        on:click={onAddNote}
+        class="block lg:hidden text-md text-zinc-500 hover:text-zinc-300 mr-5"
+        title="Menu"
+        on:click={onToggleMenu}
       >
-        <AddIcon />
+        <MenuIcon />
       </button>
+      <h1 class="flex-grow font-sans text-lg text-zinc-400">Takedown</h1>
     </div>
-    <div class="overflow-auto p-5">
-      {#if notes.length === 0}
-        <p class="text-sm text-center text-zinc-400">No Notes</p>
-      {:else}
-        <ul class="list-none">
-          {#each notes as note}
-            <li
-              class="flex items-center h-12 text-sm text-zinc-400 hover:text-zinc-200 rounded-md cursor-pointer mb-2 pl-5 pr-2 {note.id ===
-              editedNote.id
-                ? 'text-zinc-200 bg-neutral-600'
-                : ''}"
-              on:click={() => onSelectNote(note)}
-            >
-              <div class="grow text-ellipsis overflow-hidden whitespace-nowrap">
-                {editedNote.id === note.id ? title : note.title}
-              </div>
-              <div>
-                <button
-                  class="flex items-center justify-center h-10 w-10 text-zinc-500 hover:text-zinc-300 ml-2"
-                  on:click={() => onRemoveNote(note)}
+    <button
+      class="text-md text-zinc-500 hover:text-zinc-300"
+      title="New note"
+      on:click={onAddNote}
+    >
+      <AddIcon />
+    </button>
+  </div>
+  <div class="flex flex-grow w-full overflow-hidden bg-neutral-900">
+    <div
+      class="absolute h-full w-full left-0 top-0 block lg:hidden bg-black/25 z-10"
+      class:block={isMenuVisible === true}
+      class:hidden={isMenuVisible === false}
+      on:click={onToggleMenu}
+    />
+    <div
+      class="fixed w-80 h-full top-0 left-0 lg:relative lg:flex flex-col bg-neutral-800 z-20"
+      class:flex={isMenuVisible === true}
+      class:hidden={isMenuVisible === false}
+    >
+      <div class="overflow-auto p-5">
+        {#if notes.length === 0}
+          <p class="text-sm text-center text-zinc-400 my-3">No Notes</p>
+        {:else}
+          <ul class="list-none">
+            {#each notes as note}
+              <li
+                class="flex items-center h-12 text-sm text-zinc-400 hover:text-zinc-200 rounded-md cursor-pointer mb-2 pl-5 pr-2 {note.id ===
+                editedNote.id
+                  ? 'text-zinc-200 bg-neutral-600'
+                  : ''}"
+              >
+                <div
+                  class="grow text-ellipsis overflow-hidden whitespace-nowrap"
+                  on:click={() => onSelectNote(note)}
                 >
-                  <RemoveIcon />
-                </button>
-              </div>
-            </li>
-          {/each}
-        </ul>
+                  {editedNote.id === note.id ? title : note.title}
+                </div>
+                <div>
+                  <button
+                    class="flex items-center justify-center h-10 w-10 text-zinc-500 hover:text-zinc-300 ml-2"
+                    on:click={() => onRemoveNote(note)}
+                  >
+                    <RemoveIcon />
+                  </button>
+                </div>
+              </li>
+            {/each}
+          </ul>
+        {/if}
+      </div>
+    </div>
+    <div
+      class="h-full flex flex-col flex-grow lg:w-8/12 xl:w-9/12 bg-neutral-900 text-zinc-200 z-0"
+    >
+      {#if editedNote}
+        <div
+          class="flex flex-shrink-0 items-center h-14 border-b border-neutral-600 px-5"
+        >
+          <input
+            class="grow text-xl bg-transparent outline-0 focus:outline-none"
+            value={title}
+            on:input={onChange}
+          />
+          <div class="text-zinc-500">
+            {#if isSaving}
+              <SavingIcon />
+            {:else}
+              <CompleteIcon />
+            {/if}
+          </div>
+        </div>
+        <div class="relative grow overflow-hidden">
+          <Editor content={editedNote.content} on:change={onChange} />
+        </div>
       {/if}
     </div>
-  </div>
-  <div
-    class="h-full flex flex-col w-7/12 lg:w-8/12 xl:w-9/12 bg-neutral-900 text-zinc-200"
-  >
-    {#if editedNote}
-      <div
-        class="flex flex-shrink-0 items-center h-14 border-b border-neutral-600 px-5"
-      >
-        <input
-          class="grow text-xl bg-transparent outline-0 focus:outline-none"
-          value={title}
-          on:input={onChange}
-        />
-        <div class="text-zinc-500">
-          {#if saving}
-            <SavingIcon />
-          {:else}
-            <CompleteIcon />
-          {/if}
-        </div>
-      </div>
-      <div class="relative grow overflow-hidden">
-        <Editor content={editedNote.content} on:change={onChange} />
-      </div>
-    {/if}
   </div>
 </main>
